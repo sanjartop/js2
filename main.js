fa386f98e93b8b484b368f4e690f29f2fb26e4aa
smailkusnanjar97 <smailkulovsanjar97@gmail.com>
2023-02-10 14:36:02 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,37 @@
+// 1
+// let star = ''
+// for(let i = 0; i < 7; i++){
+//     star += '*'
+//     console.log(star);
+// }
+        
+// 2
+// for(let i = 1; i < 101; i++){
+//     if(i % 15 == 0){
+//         console.log('FizzВuzz');
+//     }else if(i % 5 == 0){
+//         console.log('Buzz');
+//     }else if(i % 3 == 0){
+//         console.log('Fizz');
+//     }else{
+//         console.log(i);
+//     }
+// }   
+
+// 3
+// let slash = '#';
+// let space = ' ';
+// let result = '';
+// let row = true;
+// for (let i = 0; i < 8; i++) {
+//   for (let j = 0; j < 8; j++) {
+//     if ((j+row) % 2 == 0) {
+//       result += space;
+//     } else {
+//       result += slash;
+//     }
+//   }
+//   console.log(result);
+//   result='';
+//   row = ! row;
+// }
---
+++ доп dz
@@ -0,0 +1,15 @@
+// 1
+// sql = ['alex', 'anna', 'oleg']
+// sql.push (1,2,3);
+// console.log(sql);
+
+// 2
+// sql = ['Alina', 'Sasha', 'Alex']
+// console.log(sql[0]);
+
+// 3
+// let arr1 = [1, true, false, undefined]
+// arr1.shift(0)
+// arr1.shift(1)
+// arr1.unshift("Sasha" , "Alina")
+// console.log(arr1);
